f0b061cd9a875a7a2e1e6e80826c2f9485d8852f
SUHADA BINTI SAMSUDIN <suhada.samsudin3110@gmail.com>
2025-05-13 16:45:59 +0800

    test page

---
+++ pages/test/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Test Page</h1>
+        <p>This is a test page.</p>
+        <nuxt-link to="/">Go back to Home</nuxt-link>
+        <nuxt-link to="/about">Go to About</nuxt-link>
+        <nuxt-link to="/test/other">Go to Other Test</nuxt-link>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
